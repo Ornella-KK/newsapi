df8b26a434630c5ebe904480f83c60ede9926137
Ornella-KK <ornellakankazi@gmail.com>
2020-11-21 19:53:28 +0200

    added request

--- app/request.py
+++ app/request.py
@@ -9,3 +9,21 @@ api_key = app.config['SOURCE_API_KEY']
 
 # Getting the source base url
 base_url = app.config["SOURCE_API_BASE_URL"]
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['results']:
+            source_results_list = get_sources_response['results']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
