33aa3f66f7b46f8e68127f2064de74ed49a4b32f
Ornella-KK <ornellakankazi@gmail.com>
2020-11-22 15:09:20 +0200

    added popular sources

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
+    popular_sources = get_sources('popular')
+    print(popular_sources)
     title = 'Home - Articles'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,popular = popular_sources)
