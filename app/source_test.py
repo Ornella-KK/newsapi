df001c7d76cb0c4b358a1029f2c3a4a4f2a38054
Ornella-KK <ornellakankazi@gmail.com>
2020-11-23 11:14:29 +0200

    removed parameters

--- app/source_test.py
+++ app/source_test.py
@@ -11,7 +11,7 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source('abc-news','ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.','https://abcnews.go.com','general','english','USA')
+        self.new_source = Source('abc-news','ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.','https://abcnews.go.com')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
