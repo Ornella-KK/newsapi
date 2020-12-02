0164ccd6ebfb1f97221427159aa0514ba1ac0dc5
Ornella-KK <ornellakankazi@gmail.com>
2020-12-02 12:29:29 +0200

    added property in existing object

--- app/models/source.py
+++ app/models/source.py
@@ -1,9 +1,10 @@
-class News_Source:
+class Source:
     
     #newssource class to define news objects
     
-    def __init__(self,id,name,url,category):
+    def __init__(self,id,name,description,url,category):
         self.id = id
         self.name = name
+        self.description = description
         self.url = url
         self.category = category
