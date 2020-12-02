452b7942da3c7bff53205c2b8052256dfc952046
Ornella-KK <ornellakankazi@gmail.com>
2020-12-02 12:29:17 +0200

    added property in existing object

--- app/source_test.py
+++ app/source_test.py
@@ -1,19 +1,19 @@
 import unittest
-from models import source_news
-News_Source=source_news.News_Source
+from models import source
+Source=source.Source
 
-class NewsSourceTest(unittest.TestCase):
-    
-    #Test  news source class
-    
+class SourceTest(unittest.TestCase):
+    '''
+    Test source class
+    '''
     def setUp(self):
         
         #set up method that will run before every test
         
-        self.new_news = News_Source('n','k','www.ed.com',"l")
+        self.new_source =Source('abc-news','ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.','"https://abcnews.go.com',"general")
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_news,News_Source))
+        self.assertTrue(isinstance(self.new_source,Source))
 
 if __name__ == '__main__':
     unittest.main()
