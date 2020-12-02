f69db7e8ecba8d2f24bb404698c89b0a50641b5b
Ornella-KK <ornellakankazi@gmail.com>
2020-12-02 11:14:24 +0200

    added source test

--- app/source_test.py
+++ app/source_test.py
@@ -1,21 +1,19 @@
 import unittest
-from models import source
-Source = source.Source
+from models import source_news
+News_Source=source_news.News_Source
 
-class SourceTest(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the source class
-    '''
-
+class NewsSourceTest(unittest.TestCase):
+    
+    #Test  news source class
+    
     def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
-        self.new_source = Source('abc-news','ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.','https://abcnews.go.com','general','en','us')
+        
+        #set up method that will run before every test
+        
+        self.new_news = News_Source('n','k','www.ed.com',"l")
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_source,Source))
-
+        self.assertTrue(isinstance(self.new_news,News_Source))
 
 if __name__ == '__main__':
     unittest.main()
